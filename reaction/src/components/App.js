73081baf3010f4ccff92dd1446bf90205a5a180b
Visk <Matt.Visk@elektrobit.com>
2019-12-30 13:57:16 -0500

    reaction app complete

--- reaction/src/components/App.js
+++ reaction/src/components/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import PublishMessage from './PublishMessage';
+import MessageBoard from './MessageBoard';
+import SetUsername from './SetUsername';
 
 class App extends Component {
     
     render() {
         return (
             <div>
-                <h2>React App</h2>
+                <h2>Reaction</h2>
+                <SetUsername />
+                <hr />
+                <PublishMessage />
+                <hr />
+                <MessageBoard />
             </div>
         )
     }
--- reaction/src/reducers/index.js
+++ reaction/src/reducers/index.js
@@ -1,1 +1,10 @@
-export default () => {};
+import { combineReducers } from 'redux';
+import messagesReducer from './messages';
+import usernameReducer from './username';
+import reactionsReducer from './reactions';
+
+export default combineReducers({
+    messages: messagesReducer,
+    username: usernameReducer,
+    reactions: reactionsReducer
+});
